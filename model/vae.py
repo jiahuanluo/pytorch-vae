40bba1af0d7d2359951e66052b093cf04b3bcb5d
jiahuanluo <luojiahuan001@gmail.com>
2019-09-26 14:16:26 +0800

    debug

--- model/vae.py
+++ model/vae.py
@@ -77,32 +77,6 @@ class CNN_VAE(nn.Module):
             nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
         )
-        # self.deconv1 = nn.Sequential(
-        #     nn.ConvTranspose2d()
-        # )
-        # self.encoder = nn.Sequential(
-        #     nn.Conv2d(in_channels=1, out_channels=32, kernel_size=[3,4], stride=2),
-        #     nn.ReLU(),
-        #     nn.Conv2d(in_channels=32, out_channels=64, kernel_size=[3,4], stride=2),
-        #     nn.ReLU(),
-        #     nn.Conv2d(in_channels=64, out_channels=128, kernel_size=[3,4], stride=2),
-        #     nn.ReLU(),
-        #     nn.Conv2d(in_channels=128, out_channels=256, kernel_size=[3,4], stride=2),
-        #     nn.ReLU(),
-        # )
-
-        # self.decode = nn.Sequential(
-        #     nn.ConvTranspose2d(in_channels=12288, out_channels=256, kernel_size=[3,4], stride=2),
-        #     nn.ReLU(),
-        #     nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=[3,4], stride=2),
-        #     nn.ReLU(),
-        #     nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=[3,4], stride=2),
-        #     nn.ReLU(),
-        #     nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=[3,4], stride=2),
-        #     nn.ReLU(),
-        #     nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=[3,4], stride=2),
-        #     nn.Sigmoid(),
-        # )
 
         self.fc1 = nn.Linear(17920, 32)
         self.fc2 = nn.Linear(17920, 32)
@@ -128,10 +102,6 @@ class CNN_VAE(nn.Module):
         eps = torch.randn_like(std)
         return mu + eps * std
 
-    # def decode(self, z):
-    #    h3 = F.relu(self.fc3(z))
-    #    return torch.sigmoid(self.fc4(h3))
-
     def forward(self, x):
         h = self.encoder(x)
         mu = self.fc1(h)
@@ -140,4 +110,67 @@ class CNN_VAE(nn.Module):
         z = self.fc3(z)
         z = z.view(z.size(0), 17920, 1, 1)
         print(z.shape)
+        return self.decoder(z), mu, logvar
+
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+
+class UnFlatten(nn.Module):
+    def forward(self, input, size=1024):
+        return input.view(input.size(0), size, 1, 1)
+
+
+class VAE(nn.Module):
+    def __init__(self, image_channels=1, h_dim=1024, z_dim=32):
+        super(VAE, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(image_channels, 32, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2),
+            nn.ReLU(),
+            Flatten()
+        )
+
+        self.fc1 = nn.Linear(h_dim, z_dim)
+        self.fc2 = nn.Linear(h_dim, z_dim)
+        self.fc3 = nn.Linear(z_dim, h_dim)
+
+        self.decoder = nn.Sequential(
+            UnFlatten(),
+            nn.ConvTranspose2d(h_dim, 128, kernel_size=5, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=6, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, image_channels, kernel_size=6, stride=2),
+            nn.Sigmoid(),
+        )
+
+    def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        # return torch.normal(mu, std)
+        esp = torch.randn(*mu.size())
+        z = mu + std * esp
+        return z
+
+    def bottleneck(self, h):
+        mu, logvar = self.fc1(h), self.fc2(h)
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
+
+    def representation(self, x):
+        return self.bottleneck(self.encoder(x))[0]
+
+    def forward(self, x):
+        h = self.encoder(x)
+        z, mu, logvar = self.bottleneck(h)
+        z = self.fc3(z)
         return self.decoder(z), mu, logvar
