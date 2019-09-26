5a5d073fe6993bc4a1528f7e910c5917139bee14
jiahuanluo <luojiahuan001@gmail.com>
2019-09-26 13:12:06 +0800

    update

--- model/vae.py
+++ model/vae.py
@@ -43,55 +43,101 @@ class FC_VAE(nn.Module):
 class CNN_VAE(nn.Module):
     def __init__(self):
         super(CNN_VAE, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=[3,4], stride=2),
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=[3,4], stride=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=[3,4], stride=2),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=[3,4], stride=2),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
         )
 
-        self.decode = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=12288, out_channels=256, kernel_size=[3,4], stride=2),
+        self.deconv1 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=17920, out_channels=128, kernel_size=(15, 20), stride=2),
             nn.ReLU(),
-            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=[3,4], stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=[3,4], stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=[3,4], stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=[3,4], stride=2),
-            nn.Sigmoid(),
         )
+        self.deconv2 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=(1, 1),
+                               output_padding=(1, 1)),
+            nn.ReLU(),
+        )
+        self.deconv3 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+        )
+        self.deconv4 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+        )
+        # self.deconv1 = nn.Sequential(
+        #     nn.ConvTranspose2d()
+        # )
+        # self.encoder = nn.Sequential(
+        #     nn.Conv2d(in_channels=1, out_channels=32, kernel_size=[3,4], stride=2),
+        #     nn.ReLU(),
+        #     nn.Conv2d(in_channels=32, out_channels=64, kernel_size=[3,4], stride=2),
+        #     nn.ReLU(),
+        #     nn.Conv2d(in_channels=64, out_channels=128, kernel_size=[3,4], stride=2),
+        #     nn.ReLU(),
+        #     nn.Conv2d(in_channels=128, out_channels=256, kernel_size=[3,4], stride=2),
+        #     nn.ReLU(),
+        # )
 
-        self.fc1 = nn.Linear(12288, 32)
-        self.fc2 = nn.Linear(12288, 32)
-        self.fc3 = nn.Linear(32, 12288)
+        # self.decode = nn.Sequential(
+        #     nn.ConvTranspose2d(in_channels=12288, out_channels=256, kernel_size=[3,4], stride=2),
+        #     nn.ReLU(),
+        #     nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=[3,4], stride=2),
+        #     nn.ReLU(),
+        #     nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=[3,4], stride=2),
+        #     nn.ReLU(),
+        #     nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=[3,4], stride=2),
+        #     nn.ReLU(),
+        #     nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=[3,4], stride=2),
+        #     nn.Sigmoid(),
+        # )
 
-    # def encode(self, x):
-    #     h1 = F.relu(self.fc1(x))
-    #     return self.fc21(h1), self.fc22(h1)
+        self.fc1 = nn.Linear(17920, 32)
+        self.fc2 = nn.Linear(17920, 32)
+        self.fc3 = nn.Linear(32, 17920)
+
+    def encoder(self, x):
+        h1 = self.conv1(x)  # (1, 32, 60, 80)
+        h2 = self.conv2(h1)  # (1, 64, 30, 40)
+        h3 = self.conv3(h2)  # (1, 128, 15, 20)
+        h4 = self.conv4(h3)  # (1, 256, 7, 10)
+        h = h4.view(h4.size(0), -1)
+        return h
+
+    def decoder(self, x):
+        z1 = self.deconv1(x)
+        z2 = self.deconv2(z1)
+        z3 = self.deconv3(z2)
+        z4 = self.deconv4(z3)
+        return z4
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         return mu + eps * std
 
-    #def decode(self, z):
+    # def decode(self, z):
     #    h3 = F.relu(self.fc3(z))
     #    return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
         h = self.encoder(x)
-        print(h.size())
-        h = h.view(h.size(0), -1)
         mu = self.fc1(h)
         logvar = self.fc2(h)
         z = self.reparameterize(mu, logvar)
         z = self.fc3(z)
-        z = z.view(z.size(0), 12288, 1, 1)
+        z = z.view(z.size(0), 17920, 1, 1)
         print(z.shape)
-        return self.decode(z), mu, logvar
+        return self.decoder(z), mu, logvar
